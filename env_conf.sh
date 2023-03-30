8310eee2443ef1be967335d219efd96f5a27215e
Nils Oberg <noberg@illinois.edu>
2023-03-30 10:32:55 -0500

    Added files necessary for KBase deployment.

---
+++ env_conf.sh
@@ -0,0 +1,14 @@
+
+# Path to base ShortBRED app dir, not EFI wrapper
+export SHORTBRED_APP_HOME=
+#/home/groups/efi/apps/shortbred/cgfp_2017-12-20
+export SHORTBRED_DATA_HOME=
+# Path to EFI wrapper scripts
+export EFI_SHORTBRED_HOME=
+
+export SHORTBRED_IDENTIFY=$SHORTBRED_APP_HOME/shortbred_identify.py
+export SHORTBRED_QUANTIFY=$SHORTBRED_APP_HOME/shortbred_quantify.py
+export SHORTBRED_PARSE_SSN=$SHORTBRED_DATA_HOME/scripts/parse_ssn.py
+export SHORTBRED_AGS_FILE=$SHORTBRED_DATA_HOME/AvgGenomeSizes.txt
+export SHORTBRED_MOD=
+
